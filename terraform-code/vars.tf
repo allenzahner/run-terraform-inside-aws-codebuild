8c3b70dbfc3fe4975158054cfc0453c367dcc880
Allen Zahaer <allen.zahaer@smiledirectclub.com>
2019-06-09 21:29:03 -0400

    Commit for vulcan changes
    Updated files for vulcan changes

--- terraform-code/vars.tf
+++ terraform-code/vars.tf
@@ -1,16 +1,10 @@
 variable "region" {
-  #default = "eu-central-1" # Frankfurt
-  #default = "us-east-2" # Ohio
-  #default = "us-west-2" #Oregon
-  #default = "ap-southeast-2"
-  #default = "us-east-2"
-  #default = "eu-west-1"
-  default = "us-east-1" # Virginia
+  default = "us-west-1"
   description = "the region where you want deploy the solution"
 }
 
 variable "prefix" {
-    default = "pepapp1"
+    default = "allenapp1"
     description = "The prefix used to build the elements"
 }
 
